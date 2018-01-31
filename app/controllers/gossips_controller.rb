05741e3c591dbe2fecd1c675a8c58a2cc4e80268
Simon Wyckaert <simon@digitold.com>
2018-01-31 16:42:10 +0100

    beaucoup de changements depuis 2 heures

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,6 +1,48 @@
 class GossipsController < ApplicationController
 
-  def index
+  def home
   end
-  
+
+  def index
+    @gossips = Gossip.all
+  end
+
+  def show
+    @gossip = Gossip.find(params[:id])
+
+  end
+
+  def edit
+    @gossip = Gossip.find(params[:id])
+
+  end
+
+  def update
+    @gossip = Gossip.find(params[:id])
+    @gossip.update(gossip_params)
+    redirect_to gossips_path
+  end
+
+  def new
+    @gossip = Gossip.new
+  end
+
+  def create
+    @gossip = Gossip.create(gossip_params)
+    @gossip.save
+    redirect_to @gossip
+  end
+
+  def destroy
+    @gossip = Gossip.find(params[:id])
+    @gossip.destroy
+    redirect_to gossips_path
+  end
+
+  private
+
+  def gossip_params
+    params.require(:gossip).permit(:anonymous_author, :content)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
 
-  root 'gossips#index'
-  get 'gossips/index'
+  root to: 'gossips#home'
+  get '/gossips', to: 'gossips#index'
+
+  resources :gossips
 
 end
+
+=begin
+Prefix Verb   URI Pattern                 Controller#Action
+  root GET    /                           gossips#index
+gossips_index GET    /gossips/index(.:format)    gossips#index
+gossips GET    /gossips(.:format)          gossips#index
+       POST   /gossips(.:format)          gossips#create
+new_gossip GET    /gossips/new(.:format)      gossips#new
+edit_gossip GET    /gossips/:id/edit(.:format) gossips#edit
+gossip GET    /gossips/:id(.:format)      gossips#show
+       PATCH  /gossips/:id(.:format)      gossips#update
+       PUT    /gossips/:id(.:format)      gossips#update
+       DELETE /gossips/:id(.:format)      gossips#destroy
+=end
